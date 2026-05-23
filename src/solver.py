2bb8e3ff5d18bde9a827deb2abfb8230932d84f5
Steven Reich <pokepress@gmail.com>
2026-05-22 21:04:01 -0500

    Pass phaswe weight setting to stft loss

--- src/solver.py
+++ src/solver.py
@@ -101,12 +101,12 @@ class Solver(object):
         if 'stft' in self.args.losses:
             self.mrstftloss = MultiResolutionSTFTLoss(factor_sc=args.stft_sc_factor,
                                                   factor_mag=args.stft_mag_factor,magnitude_weight_shift=args.stft_mag_weight_shift,
-                                                  factor_trans=args.stft_trans_factor).to(self.device)
+                                                  factor_trans=args.stft_trans_factor, phase_weight=args.stft_phase_weight).to(self.device)
         if 'stftcustom' in self.args.losses:
             self.mrstftlosscustom = MultiResolutionSTFTLoss(factor_sc=args.stft_sc_factor,
                                                   factor_mag=args.stft_mag_factor,start_interval=args.stftcustom_start,
                                                   end_interval=args.stftcustom_end,magnitude_weight_shift=args.stft_mag_weight_shift,
-                                                  factor_trans=args.stft_trans_factor).to(self.device)
+                                                  factor_trans=args.stft_trans_factor, phase_weight=args.stft_phase_weight).to(self.device)
 
         if 'discriminator_model' in self.args.experiment and \
                 self.args.experiment.discriminator_model == 'hifi':
